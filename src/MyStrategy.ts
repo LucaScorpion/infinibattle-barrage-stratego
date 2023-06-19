1bc31c27cf94fa58908509f17fcd485c1664ade5
Luca Scalzotto <luca@scorpiac.com>
2023-06-19 19:06:14 +0200

    Separate allMoves func

--- src/MyStrategy.ts
+++ src/MyStrategy.ts
@@ -7,13 +7,13 @@ import {
 import { GameInit } from './model/GameInit';
 import { GameState } from './model/GameState';
 import { MoveCommand } from './model/commands/MoveCommand';
-import { Cell } from './model/Cell';
-import { addCoordinates, coordinateToString, flip } from './model/Coordinate';
+import { coordinateToString, flip } from './model/Coordinate';
 import { PieceInfo } from './PieceInfo';
 import { distance } from './distance';
 import { Rank } from './model/Rank';
 import { getWinner } from './battleResult';
 import { setupOne } from './setupOne';
+import { allMoves } from './allMoves';
 
 export class MyStrategy extends Strategy {
   private availablePieces: Rank[] = [];
@@ -39,20 +39,8 @@ export class MyStrategy extends Strategy {
     this.processLastMove(state);
     this.processBattleResult(state);
 
-    // Get a map of all cells by coordinate.
-    const cellsByCoord: Record<string, Cell> = {};
-    state.Board.forEach(
-      (c) => (cellsByCoord[coordinateToString(c.Coordinate)] = c)
-    );
-
-    // Get all cells with allied pieces.
-    const myCells = state.Board.filter((c) => c.Owner === this.me);
-    // Get all possible moves for each piece.
-    const allPossibleMoves = myCells.flatMap((c) =>
-      this.getMovesForCell(cellsByCoord, c)
-    );
-
     // Pick a random move.
+    const allPossibleMoves = allMoves(this.me, state);
     const randomIndex = Math.floor(Math.random() * allPossibleMoves.length);
     return allPossibleMoves[randomIndex];
   }
@@ -60,52 +48,6 @@ export class MyStrategy extends Strategy {
   protected processOpponentMove(state: GameState): void {
     this.setupPieceInfo(state);
     this.processBattleResult(state);
-  }
-
-  private getMovesForCell(
-    cellsByCoord: Record<string, Cell>,
-    cell: Cell
-  ): MoveCommand[] {
-    if (cell.Rank === 'Flag' || cell.Rank === 'Bomb') {
-      return [];
-    }
-
-    const result: MoveCommand[] = [];
-
-    const deltas = [
-      { X: 1, Y: 0 },
-      { X: -1, Y: 0 },
-      { X: 0, Y: 1 },
-      { X: 0, Y: -1 },
-    ];
-
-    for (const delta of deltas) {
-      let target = cell.Coordinate;
-
-      let steps = 0;
-      while (steps < 1 || cell.Rank === 'Scout') {
-        steps++;
-        target = addCoordinates(target, delta);
-        const targetCell = cellsByCoord[coordinateToString(target)];
-
-        // Check if the target is out of bounds, water, or our own piece.
-        if (!targetCell || targetCell.IsWater || targetCell.Owner == this.me) {
-          break;
-        }
-
-        result.push({
-          From: cell.Coordinate,
-          To: target,
-        });
-
-        // After we encounter a piece, we can't move further.
-        if (targetCell.Owner != null) {
-          break;
-        }
-      }
-    }
-
-    return result;
   }
 
   private setupPieceInfo(state: GameState): void {
